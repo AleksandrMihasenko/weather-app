4df65e864b0ed32fb96e411a716be21e71f769aa
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-10-10 16:02:22 +0300

    add js code for getting city and weather information

---
+++ js/forecast.js
@@ -0,0 +1,39 @@
+const key = 'GDzJCuxipYxBVIgqYXSWMBkGMWwB7gmr';
+
+
+
+//get city information
+const getCity = async (city) => {
+    
+    //make queries
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    
+    //wait responses
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+//get weather information
+const getWeather = async (id) => {
+    
+    //make queries for id location
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+getCity('moscow')
+    .then(data => {
+        return getWeather(data.Key);
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err))
